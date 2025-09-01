b90a65c77c35e9077f9708605f5db02458937b32
Rusikesh8956 <rushikeshdivate7@gmail.com>
2025-09-01 15:06:40 +0530

    I have changes in 3 files

--- src/main/java/com/bookverser/BookVerse/controller/BookController.java
+++ src/main/java/com/bookverser/BookVerse/controller/BookController.java
@@ -1,9 +1,24 @@
 package com.bookverser.BookVerse.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bookverser.BookVerse.dto.BookDto;
+
+
+
+
+
 
 @RestController
 @RequestMapping("books")
 public class BookController {
 
+	@PostMapping("/api/books")
+	public String addBook(@RequestBody BookDto book) {
+		return null;
+		
+	}
 }
